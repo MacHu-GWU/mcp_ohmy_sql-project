5314e5d6d07839c40708c65566655badc71bcfaf
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-06-09 17:38:45 -0400

    fix ci

--- mcp_ohmy_sql/tests/config.py
+++ mcp_ohmy_sql/tests/config.py
@@ -4,7 +4,7 @@ import os
 
 from . import chinook
 
-from ..paths import path_config
+from ..paths import path_config, path_chinook_sqlite
 from ..constants import EnvVarEnum
 
 os.environ[EnvVarEnum.MCP_OHMY_SQL_CONFIG.name] = str(path_config)
@@ -12,3 +12,4 @@ os.environ[EnvVarEnum.MCP_OHMY_SQL_CONFIG.name] = str(path_config)
 from ..config.config_init import config
 
 chinook_db = config.databases_mapping["chinook"]
+chinook_db.connection.create_engine_kwargs = {"url": f"sqlite:///{path_chinook_sqlite}"}
