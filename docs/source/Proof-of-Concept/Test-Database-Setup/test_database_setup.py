30a4d462145086c20f0c8e7a77dc8e7ac1387118
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-06-10 12:55:03 -0400

    fix ci

--- docs/source/Proof-of-Concept/Test-Database-Setup/test_database_setup.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from mcp_ohmy_sql.tests.test_database_setup import DatabaseEnum, setup_test_database
-
-setup_test_database(engine=DatabaseEnum.sqlite)
-# setup_test_database(engine=DatabaseEnum.postgres)
