e2219f06341c7baaba770275ec036f18d1d1a5d1
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-06-10 16:14:54 -0400

    Update config_init.py

--- mcp_ohmy_sql/config/config_init.py
+++ mcp_ohmy_sql/config/config_init.py
@@ -17,5 +17,6 @@ if (
 else:
     MCP_OHMY_SQL_CONFIG = EnvVarEnum.MCP_OHMY_SQL_CONFIG.value
 
+print(f"{MCP_OHMY_SQL_CONFIG = }")
 path_mcp_ohmy_sql_config = Path(MCP_OHMY_SQL_CONFIG)
 config = Config.load(path=path_mcp_ohmy_sql_config)
