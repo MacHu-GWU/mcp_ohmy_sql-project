b7c4a7fe9f387e04eec81367b506bbcbd278aff6
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-06-18 23:57:57 -0400

    Update create_app.py

--- mcp_ohmy_sql/create_app.py
+++ mcp_ohmy_sql/create_app.py
@@ -6,7 +6,7 @@ def create_app():
 
     from .tools import list_databases
     from .tools import list_tables
-    from .tools import get_database_details
+    from .tools import get_all_database_details
     from .tools import get_schema_details
     from .tools import execute_select_statement
 
