74b320cd88a2085d43a78454c7f4ceb44bfd131e
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-06-20 00:12:05 -0400

    [no ci] add release-announcement

--- docs/poc/Test-Database-Setup/test_database_setup.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from mcp_ohmy_sql.tests.test_database_setup import EngineEnum, setup_test_database
-
-setup_test_database(engine=EngineEnum.sqlite)
-# setup_test_database(engine=EngineEnum.postgres)
